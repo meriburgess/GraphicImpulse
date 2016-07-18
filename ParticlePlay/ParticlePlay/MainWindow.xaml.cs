99ecff386c291264478860afc43e9252c738a893
Meri Burgess <Meri Burgess>
2016-07-18 00:34:09 -0600

    Added rainbow rectangle and particle files

---
+++ ParticlePlay/ParticlePlay/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParticlePlay
+{
+    class DrawingVisualElement : FrameworkElement
+    {
+        public DrawingVisual visual;
+
+        public DrawingVisualElement() { visual = new DrawingVisual(); }
+
+        protected override int VisualChildrenCount { get { return 1; } }
+
+        protected override Visual GetVisualChild(int index) { return visual; }
+    }
+
+    public class Particle
+    {
+        public Point Position;
+        public Point Velocity;
+        public Color Color;
+        public double Lifespan;
+        public double Elapsed;
+
+        public void Update(double elapsedSeconds)
+        {
+            Elapsed += elapsedSeconds;
+            if (Elapsed > Lifespan)
+            {
+                Color.A = 0;
+                return;
+            }
+            Color.A = (byte)(255 - ((255 * Elapsed)) / Lifespan);
+            Position.X += Velocity.X * elapsedSeconds;
+            Position.Y += Velocity.Y * elapsedSeconds;
+        }
+    }
+
+    public class ParticleEmitter
+    {
+        public Point Center { get; set; }
+        public List<Particle> particles = new List<Particle>();
+        Random rand = new Random();
+        public WriteableBitmap TargetBitmap;
+        public WriteableBitmap ParticleBitmap;
+
+        void CreateParticle()
+        {
+            var speed = rand.Next(800) + 140;
+            var angle = (2 * Math.PI * rand.NextDouble());
+
+
+
+            //triangle
+            //var angle = (2 * Math.PI * (rand.NextDouble() / 8)) + 1.1;
+
+            // straight down
+            //var angle = 6.25;
+
+            //straight up
+            // var angle = 3.1;
+
+            particles.Add(
+                new Particle()
+                {
+                    Position = new Point(Center.X, Center.Y),
+                    Velocity = new Point(
+                        Math.Sin(angle) * speed,
+                        Math.Cos(angle) * speed),
+                    Color = Color.FromRgb(0, 255, 0),
+                    Lifespan = 0.5 + rand.Next(20) / 1000d
+                });
+        }
+
+        public void Update()
+        {
+            var updateInterval = .001;
+
+            CreateParticle();
+
+            particles.RemoveAll(p =>
+            {
+                p.Update(updateInterval);
+                return p.Color.A == 0;
+            });
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var canvas = new Canvas();
+
+            Content = canvas;
+
+            var emitter = new ParticleEmitter();
+
+            var element = new DrawingVisualElement();
+
+            Random rnd = new Random();
+
+            CompositionTarget.Rendering += (s, e) =>
+            {
+                emitter.Update();
+
+                canvas.Children.Clear();
+
+                canvas.Children.Add(new Rectangle() { Fill = Brushes.Black, Width = canvas.ActualWidth, Height = canvas.ActualHeight });
+
+                using (var dc = element.visual.RenderOpen())
+                {
+                    //dc.DrawRectangle(
+                    //    Brushes.Black,
+                    //    null,
+                    //    new Rect(
+                    //        new Size(
+                    //            canvas.ActualWidth,
+                    //            canvas.ActualHeight)));
+
+                    emitter.particles.ForEach(p =>
+                    {
+
+                        var c = p.Color;
+
+                        c.A /= 2;
+
+                    //dc.DrawLine(new Pen(new SolidColorBrush(c),1),  p.Position, new Point(p.Position.X , p.Position.Y + rnd.Next(-100, 100) )
+                    //        );
+
+                    
+
+                        dc.DrawEllipse(
+                           new SolidColorBrush(c),
+                            null, p.Position, 7, 7);
+
+                        dc.DrawEllipse(
+                            new SolidColorBrush(p.Color),
+                            null, p.Position, 2, 2);
+                    });
+                }
+
+                canvas.Children.Add(element);
+            };
+
+            MouseMove += (s, e) => emitter.Center = e.GetPosition(canvas);
+        }
+    }
+}
---
+++ rectangleRainbowFill/rectangleRainbowFill/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace rectangleRainbowFill
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Timer myTimer = new Timer();
+        private TimeSpan myTS = new TimeSpan();
+        private int buttonClickCtr = 0;
+        private int redCtr = 0;
+        private int greenCtr = 0;
+        private int blueCtr = 0;
+
+        public MainWindow()
+        {
+            this.myTimer.Elapsed += new ElapsedEventHandler(timer_Tick);
+            this.myTimer.Interval = 1000;
+            this.myTimer.Enabled = true;
+            
+
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // System.Drawing.Color c = ColorTranslator.FromHtml("#FFCCDD");
+            //  System.Windows.Media.Color color = System.Windows.Media.Color.FromRgb(c.R, c.G, c.B);
+            //rectangleColor.Color = color;
+            byte myByte1 = Convert.ToByte(redCtr % 255);
+            byte myByte2 = Convert.ToByte(greenCtr % 255);
+            byte myByte3 = Convert.ToByte(blueCtr % 255);
+
+            byte c_R = myByte1;
+            byte c_G = myByte2;
+            byte c_B = myByte3;
+            SolidColorBrush myBrush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, c_R, c_G, c_B));
+            myRectangle.Fill = myBrush;
+            
+                redCtr += 10;
+                greenCtr += 10;
+                // blueCtr += 10;
+           
+        }
+        
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            myTS = myTS.Add(TimeSpan.FromSeconds(1));
+          //  Console.WriteLine(string.Format("{0}:{1}", myTS.Minutes, myTS.Seconds));
+            
+        }
+    }
+}
